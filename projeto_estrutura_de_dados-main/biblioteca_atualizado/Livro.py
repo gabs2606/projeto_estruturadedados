296e91c596178e9a25039cf53066ff8cf9796b24
gabs2606 <gabrielasantos2600202@gmail.com>
2025-05-25 14:56:48 -0300

    Update Livro.py

--- projeto_estrutura_de_dados-main/biblioteca_atualizado/Livro.py
+++ projeto_estrutura_de_dados-main/biblioteca_atualizado/Livro.py
@@ -1,5 +1,5 @@
 class Livro:
-    def __init__(self, titulo, autor, isbn, faixa_etaria, quantidade):
+    def __init__(self, titulo, autor, isbn, faixa_etaria, quantidade, data):
         if len(str(isbn)) != 13:
             raise ValueError("ISBN deve ter 13 dígitos!")
         self.titulo = titulo
@@ -7,7 +7,9 @@ class Livro:
         self.isbn = isbn
         self.faixa_etaria = faixa_etaria
         self.disponivel = True
-        self.quantidade = quantidade
+        self.quantidade = int(quantidade)
+        self.data = data
+
     def __str__(self):
         disponibilidade = "Disponível" if self.disponivel else "Indisponível"
         return (
@@ -16,5 +18,6 @@ class Livro:
             f"ISBN: {self.isbn}\n"
             f"Faixa Etária: {self.faixa_etaria}\n"
             f"Quantidade: {self.quantidade}\n"
+            f"Data: {self.data}\n"
             f"Status: {disponibilidade}"
         )
